3c96e3987aba676939e4fa06fafee52d60419e21
Jim Song <jim.song10@t-mobile.com>
2020-07-05 15:02:36 -0400

    adding cards to the project

--- lib/live_view_counter_web/live/counter.ex
+++ lib/live_view_counter_web/live/counter.ex
@@ -3,6 +3,7 @@ defmodule LiveViewCounterWeb.Counter do
   alias LiveViewCounter.Count
   alias Phoenix.PubSub
   alias LiveViewCounter.Presence
+  alias LiveViewCounter.Cards
 
   @topic Count.topic
   @presence_topic "presence"
@@ -13,11 +14,14 @@ defmodule LiveViewCounterWeb.Counter do
     Presence.track(self(), @presence_topic, socket.id, %{})
     LiveViewCounterWeb.Endpoint.subscribe(@presence_topic)
 
+
     initial_present =
       Presence.list(@presence_topic)
       |> map_size
 
-    {:ok, assign(socket, val: Count.current(), present: initial_present) }
+    [top_card | cards] = Cards.create_shuffled_deck()
+
+    {:ok, assign(socket, val: Count.current(), present: initial_present, cards: cards, top_card: top_card, card_count: length(cards)) }
   end
 
   def handle_event("inc", _, socket) do
@@ -26,6 +30,11 @@ defmodule LiveViewCounterWeb.Counter do
 
   def handle_event("dec", _, socket) do
     {:noreply, assign(socket, :val, Count.decr())}
+  end
+
+  def handle_event("next_card", _, %{assigns: %{cards: cards}} = socket ) do
+    [next_card | new_cards] = cards
+    {:noreply, assign(socket, cards: new_cards, top_card: next_card, card_count: length(new_cards))}
   end
 
   def handle_info({:count, count}, socket) do
@@ -47,8 +56,11 @@ defmodule LiveViewCounterWeb.Counter do
       <h1>The count is: <%= @val %></h1>
       <button phx-click="dec">-</button>
       <button phx-click="inc">+</button>
+      <button phx-click="next_card">next</button>
       <h1>Current users: <%= @present %></h1>
-    </div>
+      <h1>Current card: <%= @top_card %></h1>
+      <h1>Cards Left: <%= @card_count %></h1>
+    </div> .
     """
   end
 end
